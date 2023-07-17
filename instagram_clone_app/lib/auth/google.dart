0be82f348aa2dc6e2b95c9d5ca8195cb7c92a04b
Angelo <angelolouis.baricuatro.20@usjr.edu.ph>
2023-07-17 11:41:53 +0800

    added missing files and classes

--- instagram_clone_app/lib/auth/google.dart
+++ instagram_clone_app/lib/auth/google.dart
@@ -1,58 +1,58 @@
-import 'package:flutter/material.dart';
-import 'package:firebase_auth/firebase_auth.dart';
-import 'package:font_awesome_flutter/font_awesome_flutter.dart';
-import 'package:google_sign_in/google_sign_in.dart';
-
-import '../pages/homepage.dart'; // Import your HomePage class
-
-class GoogleSignInButton extends StatefulWidget {
-  const GoogleSignInButton({Key? key}) : super(key: key);
-
-  @override
-  State<GoogleSignInButton> createState() => _GoogleSignInButtonState();
-}
-
-class _GoogleSignInButtonState extends State<GoogleSignInButton> {
-  final GoogleSignIn googleSignIn = GoogleSignIn();
-  final FirebaseAuth auth = FirebaseAuth.instance;
-
-  Future<void> signInWithGoogle(BuildContext context) async {
-      // Trigger the Google Sign-In flow
-      final GoogleSignInAccount? googleUser = await googleSignIn.signIn();
-
-      if (googleUser != null) {
-        // Obtain the Google Authentication details
-        final GoogleSignInAuthentication googleAuth =
-            await googleUser.authentication;
-
-        // Create a new credential
-        final AuthCredential credential = GoogleAuthProvider.credential(
-          idToken: googleAuth.idToken,
-          accessToken: googleAuth.accessToken,
-        );
-
-        // Sign in to Firebase with the Google credential
-        UserCredential result = await auth.signInWithCredential(credential);
-        User? user = result.user;
-
-        // Navigate to HomePage upon successful sign-in
-        if (user != null) {
-          Navigator.of(context).pushReplacement(
-            MaterialPageRoute(builder: (context) => HomePage()),
-          );
-        }
-      }
-    }
-
-  @override
-  Widget build(BuildContext context) {
-    return ElevatedButton.icon(
-      onPressed: () => signInWithGoogle(context),
-      // Pass the BuildContext to the signInWithGoogle method
-      icon: const FaIcon(
-        FontAwesomeIcons.google,
-      ),
-      label: Text('Sign in with Google'),
-    );
-  }
-}
+// import 'package:flutter/material.dart';
+// import 'package:firebase_auth/firebase_auth.dart';
+// import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+// import 'package:google_sign_in/google_sign_in.dart';
+//
+// import '../pages/homepage.dart'; // Import your HomePage class
+//
+// class GoogleSignInButton extends StatefulWidget {
+//   const GoogleSignInButton({Key? key}) : super(key: key);
+//
+//   @override
+//   State<GoogleSignInButton> createState() => _GoogleSignInButtonState();
+// }
+//
+// class _GoogleSignInButtonState extends State<GoogleSignInButton> {
+//   final GoogleSignIn googleSignIn = GoogleSignIn();
+//   final FirebaseAuth auth = FirebaseAuth.instance;
+//
+//   Future<void> signInWithGoogle(BuildContext context) async {
+//       // Trigger the Google Sign-In flow
+//       final GoogleSignInAccount? googleUser = await googleSignIn.signIn();
+//
+//       if (googleUser != null) {
+//         // Obtain the Google Authentication details
+//         final GoogleSignInAuthentication googleAuth =
+//             await googleUser.authentication;
+//
+//         // Create a new credential
+//         final AuthCredential credential = GoogleAuthProvider.credential(
+//           idToken: googleAuth.idToken,
+//           accessToken: googleAuth.accessToken,
+//         );
+//
+//         // Sign in to Firebase with the Google credential
+//         UserCredential result = await auth.signInWithCredential(credential);
+//         User? user = result.user;
+//
+//         // Navigate to HomePage upon successful sign-in
+//         if (user != null) {
+//           Navigator.of(context).pushReplacement(
+//             MaterialPageRoute(builder: (context) => HomePage()),
+//           );
+//         }
+//       }
+//     }
+//
+//   @override
+//   Widget build(BuildContext context) {
+//     return ElevatedButton.icon(
+//       onPressed: () => signInWithGoogle(context),
+//       // Pass the BuildContext to the signInWithGoogle method
+//       icon: const FaIcon(
+//         FontAwesomeIcons.google,
+//       ),
+//       label: Text('Sign in with Google'),
+//     );
+//   }
+// }
